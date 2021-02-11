6bb574a6ec315e2942f38ab873d09ded60857c4e
foie0222 <daikinoue0222@gmail.com>
2021-02-11 18:01:04 +0900

    RaceCoursekクラスを追加

--- yumachan.py
+++ yumachan.py
@@ -3,9 +3,9 @@ from scraper import Scraper
 
 
 class YumaChan:
-    def __init__(self, opdt, rcoursecd, rno, rname, horse_list):
+    def __init__(self, opdt, race_course, rno, rname, horse_list):
         self.opdt = opdt
-        self.rcoursecd = rcoursecd
+        self.race_course = race_course
         self.rno = rno
         self.rname = rname
         self.horse_list = horse_list
@@ -17,39 +17,46 @@ def get_yumachan():
     png_url = scraper.get_png_url()
 
     opdt = get_opdt(header_txt)
-    rcoursecd = get_rcoursecd(header_txt[:15])
+    race_course = get_race_course(header_txt[:15])
     rno = get_rno(header_txt)
     rname = get_rname(header_txt)
     horser_list = get_horse_list_from_png_by_google(png_url)
 
-    return YumaChan(opdt, rcoursecd, rno, rname, horser_list)
+    return YumaChan(opdt, race_course, rno, rname, horser_list)
 
 
 def get_opdt(row):
     return row[0:8]
 
 
-def get_rcoursecd(row):
+class RaceCource:
+    def __init__(self, name, roman, cd):
+        self.name = name
+        self.roman = roman
+        self.cd = cd
+
+
+def get_race_course(row):
     if '札幌' in row:
-        return 'SAPPORO'
+        return RaceCource('札幌', 'SAPPORO', '01')
     if '函館' in row:
-        return 'HAKODATE'
+        return RaceCource('函館', 'HAKODATE', '02')
     if '福島' in row:
-        return 'FUKUSHIMA'
+        return RaceCource('福島', 'FUKUSHIMA', '03')
     if '新潟' in row:
-        return 'NIIGATA'
+        return RaceCource('新潟', 'NIIGATA', '04')
     if '東京' in row:
-        return 'TOKYO'
+        return RaceCource('東京', 'TOKYO', '05')
     if '中山' in row:
-        return 'NAKAYAMA'
+        return RaceCource('中山', 'NAKAYAMA', '06')
     if '中京' in row:
-        return 'CHUKYO'
+        return RaceCource('中京', 'CHUKYO', '07')
     if '京都' in row:
-        return 'KYOTO'
+        return RaceCource('京都', 'KYOTO', '08')
     if '阪神' in row:
-        return 'HANSHIN'
+        return RaceCource('阪神', 'HANSHIN', '09')
     if '小倉' in row:
-        return 'KOKURA'
+        return RaceCource('小倉', 'KOKURA', '10')
     return None
 
 
