5158f226fa335429638a16716735f681647594c1
foie0222 <daikinoue0222@gmail.com>
2021-02-27 02:57:06 +0900

    馬単の購入ロジック追加

--- ticket.py
+++ ticket.py
@@ -33,6 +33,7 @@ def make_ticket(yumachan, odds_manager, target_refund):
     ticket_list = []
     ticket_list.extend(make_tan_ticket(yumachan, odds_manager.tan_odds_list, target_refund))
     ticket_list.extend(make_umaren_ticket(yumachan, odds_manager.umaren_odds_list, target_refund))
+    ticket_list.extend(make_umatan_ticket(yumachan, odds_manager.umatan_odds_list, target_refund))
     return ticket_list
 
 
@@ -91,17 +92,53 @@ def make_umaren_ticket(yumachan, umaren_odds_list, target_refund):
     return sorted_umaren_ticket_list
 
 
+# 馬単購入
+def make_umatan_ticket(yumachan, umatan_odds_list, target_refund):
+    umatan_ticket_list = []
+    for horse1 in yumachan.horse_list:
+        for horse2 in yumachan.horse_list:
+            if horse1.umano == horse2.umano:
+                continue
+            pair_num = horse1.umano + '-' + horse2.umano
+            odds = [umatan_odds for umatan_odds in umatan_odds_list if umatan_odds.umano == pair_num][0].odds
+            probability = get_umatan_probability(horse1.probability, horse2.probability)
+            expected_value = probability * odds
+            if expected_value >= 2:
+                bet = calc_bet(odds, target_refund)
+            else:
+                continue
+
+            ticket = Ticket(
+                yumachan,
+                'UMATAN',
+                pair_num,
+                bet,
+                expected_value,
+                probability,
+                odds)
+            umatan_ticket_list.append(ticket)
+
+    sorted_umatanticket_list = sorted(umatan_ticket_list, key=lambda t: t.number)
+    return sorted_umatanticket_list
+
+
 # 単勝の期待値を計算する
 def get_tan_probability(probability):
     return probability / 100
 
 
+# 馬単の期待値を計算する
+def get_umatan_probability(probability1, probability2):
+    _1_2_probability = probability1 * (probability2 / (100 - probability1))
+    return _1_2_probability / 100
+
+
 # 馬連の期待値を計算する
 def get_umaren_probability(probability1, probability2):
-    _1_2_probability = probability1 * (probability2 / (100 - probability1))
-    _2_1_probability = probability2 * (probability1 / (100 - probability2))
+    _1_2_probability = get_umatan_probability(probability1, probability2)
+    _2_1_probability = get_umatan_probability(probability2, probability1)
     total_probability = _1_2_probability + _2_1_probability
-    return total_probability / 100
+    return total_probability
 
 
 # 指定した払い戻し額に届くように購入金額を計算する
