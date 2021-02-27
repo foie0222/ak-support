6f0c229233f118364f526baeebb4e9ba06d6cdfa
foie0222 <daikinoue0222@gmail.com>
2021-02-27 13:08:37 +0900

    デバッグを解除

--- ipatgo.py
+++ ipatgo.py
@@ -5,8 +5,6 @@ import config
 def vote(timestamp):
     print('投票中...')
     try:
-        print('executed command:')
-        print(rf'.\ipatgo\ipatgo.exe file {config.INET_ID} {config.SUBSCRIBER_NUMBER} {config.PIN_NUMBER} {config.P_ARS_NUMBER} .\tickets\ticket_' + timestamp + '.csv')
         return os.system(rf'.\ipatgo\ipatgo.exe file {config.INET_ID} {config.SUBSCRIBER_NUMBER} {config.PIN_NUMBER} {config.P_ARS_NUMBER} .\tickets\ticket_' + timestamp + '.csv')
     except Exception as e:
         print(e.args)
