d123763805cfb2e8a0cfa135d31f6fff103b2c34
foie0222 <daikinoue0222@gmail.com>
2021-03-13 00:48:14 +0900

    debug

--- ipatgo.py
+++ ipatgo.py
@@ -6,6 +6,7 @@ def vote(csv_list, number):
     for csv in csv_list:
         print(f'投票中...{csv}')
         try:
+            print(rf'.\ipatgo\ipatgo.exe file {config.IPATS[number]["INET_ID"]} {config.IPATS[number]["SUBSCRIBER_NUMBER"]} {config.IPATS[number]["PIN_NUMBER"]} {config.IPATS[number]["P_ARS_NUMBER"]} ' + csv)
             os.system(rf'.\ipatgo\ipatgo.exe file {config.IPATS[number]["INET_ID"]} {config.IPATS[number]["SUBSCRIBER_NUMBER"]} {config.IPATS[number]["PIN_NUMBER"]} {config.IPATS[number]["P_ARS_NUMBER"]} ' + csv)
         except Exception as e:
             print(e.args)
